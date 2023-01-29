e7a544377472de35ebf274b6f75e54d05b772cdc
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2023-01-29 21:51:55 +0000

    Print Hello World Phonebook in console

--- Phonebook/Program.cs
+++ Phonebook/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Phonebook");
+        }
+    }
+}
