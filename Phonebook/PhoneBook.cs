1dbe34846f0bca3789d5cf7e960fa7079b751deb
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2023-02-04 23:40:33 +0000

    function to display contacts

--- Phonebook/PhoneBook.cs
+++ Phonebook/PhoneBook.cs
@@ -12,6 +12,27 @@ namespace Phonebook
         {
             _contacts.Add(contact);
         }
+
+        public void DisplayContact(string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found:(");
+            }
+            else
+            {
+                Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+            }
+        }
+
+        public void DisplayAllContact()
+        {
+            foreach (var contact in _contacts)
+            {
+                Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+            }
+        }
 	}
 }
 
